eb6c2b9924f9a07f77aa48e81127d5c8a937b77f
kobycleves <kitolandia-2000@hotmail.com>
2019-03-19 15:32:18 -0500

    Primer commit
    carpetas y el documento con sus diagramas y funciones

---
+++ src/model/Client.java
@@ -0,0 +1,16 @@
+public class Client(){
+
+	private String name;
+	private int id;
+	private String address;
+	private int telephoneContact;
+
+public Client (String nam, int id, String addr, int teleContact){
+	
+	name = nam;
+	this.id = id;
+	address = addr;
+	telephoneContact = teleContact;
+	
+}
+}
